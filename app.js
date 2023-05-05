227e74ccd42df4b40aeedfb7f1b881d2b4b6f3ca
Alok kumar <alokkumar2297812@gmail.com>
2023-05-05 13:45:47 +0530

    Add the delete functinality and connect it to db

--- app.js
+++ app.js
@@ -1,50 +1,90 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongooose = require("mongoose");
 const app = express();
-let items=["Buy food","Cook food","Eat food"];
-let worksItems=[];
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.set('view engine','ejs');
 
-app.get("/",function(req,res){
-    let today = new Date();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+mongooose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser :true});
 
-    let day = today.toLocaleDateString("en-US",options);
-    res.render("list",{listTitle:day, newListItems:items});
+const itemSchema = {
+    name : String
+};
+const Item = mongooose.model("Item",itemSchema);
+
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
 });
 
-app.post("/",function(req,res){
-    let item = req.body.newItem;
-
-    if(req.body.list==="Work"){
-        worksItems.push(item);
-        res.redirect("/work");
-    }else{
-        items.push(item);
-        res.redirect("/");
-    }
+const item2 = new Item({
+    name: "Hit the + button to add a new item"
 });
 
-
-app.get("/work",function(req,res){
-    res.render("list",{listTitle: "Work List",newListItems:worksItems});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item" 
 });
 
-app.post("/work",function(req,res){
-    let item=res.body.newItem;
-    worksItems.push(item);
+const defaultItems=[item1,item2,item3];
+
+app.get("/", function(req, res) {
+   Item.find().then(function (foundItems) {
+      if(foundItems.length===0){                               // checking the array initally 0 and only add the data one time 
+         Item.insertMany(defaultItems).then(function () {
+            console.log("Successfully saved defult items to DB");
+          }).catch(function (err) {
+            console.log(err);
+          });
+          res.redirect("/");        
+      }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+      }
+   }).catch(function (err) {
+     console.log(err);
+   });
+ });
+
+ app.post("/", function(req, res) {
+    const itemName = req.body.newItem;
+    const item = new Item({
+       name: itemName
+    });
+    item.save();
+    res.redirect("/");
+ });
+ 
+ app.post("/delete",function(req,res){
+   const CheckItemId = req.body.checkbox;
+   Item.findByIdAndRemove(CheckItemId).then(function(){
+      console.log("Sucessfully deleted the checked items");
+      res.redirect("/");
+   }).catch(function(err){
+      console.log(err);
+   });
+ });
+
+app.get("/work", function(req, res) {
+    Item.find({}, function(err, foundItems) {
+       if (err) {
+          console.log(err);
+       } else {
+          res.render("list", {listTitle: "Work List", newListItems: foundItems});
+       }
+    });
+ });
+
+ app.post("/work", function(req, res) {
+    const itemName = req.body.newItem;
+    const item = new Item({
+       name: itemName
+    });
+    item.save();
     res.redirect("/work");
-});
+ });
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
